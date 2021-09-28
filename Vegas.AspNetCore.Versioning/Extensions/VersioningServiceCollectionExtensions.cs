2b5c9eea0f5f4128c97f08da931bdb0931d43761
Kenan Nur <kenannur@hotmail.com>
2021-09-29 00:49:47 +0300

    Vegas.AspNetCore.Versioning project created

---
+++ Vegas.AspNetCore.Versioning/Extensions/VersioningServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Vegas.AspNetCore.Versioning.Extensions
+{
+    public static class VersioningServiceCollectionExtensions
+    {
+        public static void AddApiVersioning<TApiVersionReader>(this IServiceCollection services, string version = default)
+            where TApiVersionReader : IApiVersionReader
+        {
+            var isNotEmpty = !string.IsNullOrWhiteSpace(version);
+            var isParsable = ApiVersion.TryParse(version, out ApiVersion apiVersion);
+            var defaultVersion = isNotEmpty && isParsable ? apiVersion : ApiVersion.Default;
+
+            services.AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = defaultVersion;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ReportApiVersions = true;
+                options.ApiVersionReader = Activator.CreateInstance<TApiVersionReader>();
+            });
+        }
+    }
+}
