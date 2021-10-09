e50d3a8a14d6fe5a081932272a37736e24b96eda
Kenan Nur <kenannur@hotmail.com>
2021-10-09 20:36:27 +0300

    ConfigureAndUseSerilog for AWS Cloud watch extension

--- Vegas.AspNetCore.Logger/Serilog/SerilogBuilderExtensions.cs
+++ Vegas.AspNetCore.Logger/Serilog/SerilogBuilderExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using AWS.Logger;
+using AWS.Logger.SeriLog;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
@@ -10,11 +12,13 @@ namespace Vegas.AspNetCore.Logger.Serilog
 {
     public static class SerilogBuilderExtensions
     {
-        public static IHostBuilder ConfigureAndUseSerilog(this IHostBuilder hostBuilder)
+        public static IHostBuilder ConfigureAndUseSerilog(this IHostBuilder hostBuilder, string awsLogGroupName)
         {
             try
             {
-                ConfigureSerilog();
+                Log.Logger = GetLoggerConfiguration()
+                    .WriteTo.AWSSeriLog(new AWSLoggerConfig(awsLogGroupName))
+                    .CreateLogger();
 
                 hostBuilder = hostBuilder.ConfigureLogging(logging => logging.ClearProviders())
                                          .UseSerilog();
@@ -28,22 +32,38 @@ namespace Vegas.AspNetCore.Logger.Serilog
             return hostBuilder;
         }
 
-        private static void ConfigureSerilog()
+        public static IHostBuilder ConfigureAndUseSerilog(this IHostBuilder hostBuilder)
         {
-            var assemblyLocation = Assembly.GetExecutingAssembly().Location;
-            var directoryName = Path.GetDirectoryName(assemblyLocation);
-            var logFilePath = $"{directoryName}/Log/log.txt";
+            try
+            {
+                var assemblyLocation = Assembly.GetExecutingAssembly().Location;
+                var directoryName = Path.GetDirectoryName(assemblyLocation);
+                var logFilePath = $"{directoryName}/Log/log.txt";
 
-            Log.Logger = new LoggerConfiguration()
+                Log.Logger = GetLoggerConfiguration()
+                    .WriteTo.File(logFilePath)
+                    .CreateLogger();
+
+                hostBuilder = hostBuilder.ConfigureLogging(logging => logging.ClearProviders())
+                                         .UseSerilog();
+
+                Log.Information("Serilog configured");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "While Serilog configuring an error occured");
+            }
+            return hostBuilder;
+        }
+
+        private static LoggerConfiguration GetLoggerConfiguration()
+        {
+            return new LoggerConfiguration()
                 .MinimumLevel.Information()
                 .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
                 .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
                 .MinimumLevel.Override("System", LogEventLevel.Error)
-                .Enrich.FromLogContext()
-                //.Enrich.WithProperty("AppName", "AppName")
-                //.Enrich.WithProperty("Environment", "DEV")
-                .WriteTo.File(logFilePath)
-                .CreateLogger();
+                .Enrich.FromLogContext();
         }
     }
 }
