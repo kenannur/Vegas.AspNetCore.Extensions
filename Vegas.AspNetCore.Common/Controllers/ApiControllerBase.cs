3b4e58f030b06968a0041ac29adab9b46a27485e
Kenan Nur <kenannur@hotmail.com>
2021-10-09 22:49:56 +0300

    ApiControllerBase added

---
+++ Vegas.AspNetCore.Common/Controllers/ApiControllerBase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vegas.AspNetCore.Common.Models;
+
+namespace Vegas.AspNetCore.Common.Controllers
+{
+    [ApiController]
+    [Route("v{version:apiVersion}/[controller]/[action]")]
+    public abstract class ApiControllerBase : ControllerBase
+    {
+        protected ApiControllerBase() { }
+
+        protected IActionResult OkResponse<TMainResponse>(TMainResponse mainResponse) => CreateOkResponse(mainResponse);
+
+        protected IActionResult OkResponse() => CreateOkResponse(new VoidResponse());
+
+        private IActionResult CreateOkResponse<TMainResponse>(TMainResponse mainResponse)
+        {
+            return Ok(new ApiResponse
+            {
+                IsSuccess = true,
+                MainResponse = mainResponse
+            });
+        }
+    }
+}
