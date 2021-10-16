eefcda5a23826215a6348765a2a8c0e85accb2ee
Kenan Nur <kenannur@hotmail.com>
2021-10-17 00:07:54 +0300

    RouteBuilderExtensions added

---
+++ Vegas.AspNetCore.Common/DependencyInjection/RouteBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Vegas.AspNetCore.Common.DependencyInjection
+{
+    public static class RouteBuilderExtensions
+    {
+        public static void MapDefaults(this IRouteBuilder endpoints, IWebHostEnvironment env)
+        {
+            endpoints.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync($"{env.ApplicationName} is running: {DateTime.Now}");
+            });
+            endpoints.MapGet("/ex", context =>
+            {
+                throw new Exception("message");
+            });
+        }
+
+    }
+}
