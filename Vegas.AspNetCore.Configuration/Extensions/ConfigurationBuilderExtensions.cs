02f285747c11fc2363a3b5d070899ff3b9718390
Kenan Nur <kenannur@hotmail.com>
2021-10-16 20:25:21 +0300

    ConfigurationBuilderExtensions added

---
+++ Vegas.AspNetCore.Configuration/Extensions/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Vegas.AspNetCore.Configuration.Extensions
+{
+    public static class ConfigurationBuilderExtensions
+	{
+		public static IConfigurationBuilder AddJsonString(this IConfigurationBuilder configurationBuilder, string json)
+		{
+			return configurationBuilder.Add(new JsonStringConfigurationSource(json));
+		}
+	}
+
+	public class JsonStringConfigurationSource : IConfigurationSource
+	{
+		private readonly string _json;
+		public JsonStringConfigurationSource(string json) => _json = json;
+
+		public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+			return new JsonStringConfigurationProvider(_json);
+		}
+	}
+
+	public class JsonStringConfigurationProvider : ConfigurationProvider
+	{
+		private readonly string _json;
+		public JsonStringConfigurationProvider(string json) => _json = json;
+
+		public override void Load()
+        {
+			//Data = JsonConvert.DeserializeObject<Dictionary<string, string>>(_json);
+		}
+	}
+}
