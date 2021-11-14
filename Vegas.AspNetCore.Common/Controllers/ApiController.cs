b85db70363e82cd43e0e2bc993c588504e1cd1b9
Kenan Nur <kenannur@hotmail.com>
2021-11-15 00:32:09 +0300

    Update ApiController.cs

--- Vegas.AspNetCore.Common/Controllers/ApiController.cs
+++ Vegas.AspNetCore.Common/Controllers/ApiController.cs
@@ -8,18 +8,29 @@ namespace Vegas.AspNetCore.Common.Controllers
     {
         protected ApiController() { }
 
-        protected IActionResult OkResponse<TMainResponse>(TMainResponse mainResponse) => CreateOkResponse(mainResponse);
-
+        protected IActionResult OkResponse<TResponse>(TResponse response) => CreateOkResponse(response);
         protected IActionResult OkResponse() => CreateOkResponse(new VoidResponse());
-
-        private IActionResult CreateOkResponse<TMainResponse>(TMainResponse mainResponse)
+        private IActionResult CreateOkResponse<TResponse>(TResponse response) => Ok(new ApiResponse
         {
-            return Ok(new ApiResponse
-            {
-                IsSuccess = true,
-                MainResponse = mainResponse
-            });
-        }
+            IsSuccess = true,
+            MainResponse = response
+        });
+
+        protected IActionResult AcceptedResponse<TResponse>(TResponse response) => CreateAcceptedResponse(response);
+        protected IActionResult AcceptedResponse() => CreateAcceptedResponse(new VoidResponse());
+        private IActionResult CreateAcceptedResponse<TResponse>(TResponse response) => Accepted(new ApiResponse
+        {
+            IsSuccess = true,
+            MainResponse = response
+        });
+
+        protected IActionResult CreatedResponse<TResponse>(TResponse response) => CreateCreatedResponse(response);
+        protected IActionResult CreatedResponse() => CreateCreatedResponse(new VoidResponse());
+        private IActionResult CreateCreatedResponse<TResponse>(TResponse response) => Created("", new ApiResponse
+        {
+            IsSuccess = true,
+            MainResponse = response
+        });
     }
 
 
