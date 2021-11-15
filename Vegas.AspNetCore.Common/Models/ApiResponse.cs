133d7e7a082b682443502217a56757ae9003eb45
Kenan Nur <kenannur@hotmail.com>
2021-11-15 21:34:26 +0300

    Update ApiResponse.cs

--- Vegas.AspNetCore.Common/Models/ApiResponse.cs
+++ Vegas.AspNetCore.Common/Models/ApiResponse.cs
@@ -5,12 +5,16 @@ namespace Vegas.AspNetCore.Common.Models
     public class ApiResponse : ApiResponse<object>
     { }
 
-    public class ApiResponse<TPayload> where TPayload : class
+    public class ApiResponse<TResponse> where TResponse : class
     {
         public bool IsSuccess { get; set; }
-
         public List<string> Messages { get; set; } = new List<string>();
+        public TResponse MainResponse { get; set; }
 
-        public TPayload MainResponse { get; set; }
+        public static ApiResponse<TResponse> Success(TResponse response) => new()
+        {
+            IsSuccess = true,
+            MainResponse = response
+        };
     }
 }
